c1249e48290c80ea13d01186db9d24f33730cab3
Nevill Wilder <wildercontact@icloud.com>
2021-06-17 17:53:43 -0700

    Get specific keys from Json, even if they are nested.

--- Sources/ScopedJsonDecoder/ScopedJsonDecoder.swift
+++ Sources/ScopedJsonDecoder/ScopedJsonDecoder.swift
@@ -1,6 +1,93 @@
-public struct ScopedJsonDecoder {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+extension JSONDecoder {
+    private struct ScopedJsonDecoder {
+        private static var nestingKey: CodingUserInfoKey {
+            CodingUserInfoKey(rawValue: "nestedContainerKey")!
+        }
+
+        private static var rawNestingKey: String {
+            "nestedContainerKey"
+        }
+        private struct Key: CodingKey {
+            let stringValue: String
+            init?(stringValue: String) {
+                self.stringValue = stringValue
+                self.intValue = nil
+            }
+
+            let intValue: Int?
+            init?(intValue: Int) {
+                return nil
+            }
+        }
+
+        private struct ModelResponse<NestedModel: Decodable>: Decodable {
+            let nested: NestedModel
+
+            public init(from decoder: Decoder) throws {
+
+                // Split nested paths with '.'
+                var keyPaths = (decoder.userInfo[nestingKey] as! String)
+                    .split(separator: ".")
+
+                // Get last key to extract in the end
+                let lastKey = keyPaths.popLast()
+
+                // Loop getting container until reach final one
+                var targetContainer = try decoder.container(keyedBy: Key.self)
+
+                try keyPaths.forEach {k in
+                    targetContainer = try targetContainer.nestedContainer(keyedBy: Key.self, forKey: Key(stringValue: String(k))!)
+                }
+
+                nested = try targetContainer.decode(NestedModel.self, forKey: Key(stringValue: String(lastKey!))!)
+            }
+        }
+
+        func decode<T: Decodable>(_ type: T.Type, scopedToKey key: String, from data: Data, using decoder: JSONDecoder) -> Result<T,Error> {
+            Result {
+                decoder.userInfo[ScopedJsonDecoder.nestingKey] = key
+
+                return try decoder.decode(ModelResponse<T>.self, from: data).nested
+            }
+        }
+    }
+}
+
+extension JSONDecoder {
+    func decode<T:Decodable>(
+        _ type: T.Type,
+        scopedToKey key: String,
+        from data: Data
+    ) -> Result<T, Error> {
+        ScopedJsonDecoder().decode(
+            type.self,
+            scopedToKey: key,
+            from: data,
+            using: self)
+    }
+}
+
+extension Data{
+    func decode<T:Decodable>(
+        _ type: T.Type,
+        scopedToKey key: String,
+        _ keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy? = nil
+    ) -> Result<T, Error> {
+        let decoder = JSONDecoder()
+        guard let strategy = keyDecodingStrategy else {
+            return decoder.decode(
+                type.self,
+                scopedToKey: key,
+                from: self
+            )
+        }
+        decoder.keyDecodingStrategy = strategy
+        return decoder.decode(
+            type.self,
+            scopedToKey: key,
+            from: self
+        )
     }
 }
--- Tests/ScopedJsonDecoderTests/ScopedJsonDecoderTests.swift
+++ Tests/ScopedJsonDecoderTests/ScopedJsonDecoderTests.swift
@@ -2,10 +2,111 @@ import XCTest
 @testable import ScopedJsonDecoder
 
 final class ScopedJsonDecoderTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ScopedJsonDecoder().text, "Hello, World!")
+    let testJson = """
+    {
+    "HappyPath": {
+        "firstKey": "a",
+        "secondKey": "b",
+        "thirdKey": "c"
+    },
+    "SnakeCamelMix": {
+        "camelKey": "e",
+        "snake_key": "f",
+        "double_snake_key": "g"
+    },
+    "outerContainer": {
+        "nestedContainer": {
+                    "firstKey": "nested a",
+                    "secondKey": "nested b",
+                    "thirdKey": "nested c",
+            "innerNestedContainer": {
+                        "camelKey": "inner e",
+                        "snake_key": "inner f",
+                        "double_snake_key": "inner g"
+            }
+        },
+        "name": "aunnnn"
+    }
+    }
+    """
+        .data(using: .utf8)!
+
+
+    struct HappyPath: Codable,Equatable {
+        let firstKey: String
+        let secondKey: String
+        let thirdKey: String
+    }
+
+    struct SnakeCamelMix: Codable,Equatable {
+        let camelKey: String
+        let snakeKey: String
+        let doubleSnakeKey: String
+    }
+
+    func testHappyPath() throws {
+        let result = try testJson.decode(HappyPath.self, scopedToKey: "HappyPath").get()
+        XCTAssert(
+            result
+            ==
+            HappyPath(
+                firstKey: "a",
+                secondKey: "b",
+                thirdKey: "c"
+            )
+        )
+
+    }
+
+    func testSnakeCamelMix() throws {
+        let result = try testJson.decode(
+            SnakeCamelMix.self,
+            scopedToKey: "SnakeCamelMix",
+            .convertFromSnakeCase
+        ).get()
+
+        XCTAssert(
+            result
+            ==
+            SnakeCamelMix(
+                camelKey: "e",
+                snakeKey: "f",
+                doubleSnakeKey: "g"
+            )
+        )
+    }
+
+    func testOuterNest() throws {
+        let result = try testJson.decode(
+            HappyPath.self,
+            scopedToKey: "outerContainer.nestedContainer"
+        )
+        .get()
+
+        XCTAssert(
+            result
+            ==
+            HappyPath(
+                firstKey: "nested a",
+                secondKey: "nested b",
+                thirdKey: "nested c"
+            )
+        )
+    }
+    func testInnerNest() throws {
+        let result = try testJson.decode(
+            SnakeCamelMix.self,
+            scopedToKey: "outerContainer.nestedContainer.innerNestedContainer",
+            .convertFromSnakeCase
+        ).get()
+
+        XCTAssert(
+            result
+            ==
+            SnakeCamelMix(
+                camelKey: "inner e",
+                snakeKey: "inner f",
+                doubleSnakeKey: "inner g")
+        )
     }
 }
